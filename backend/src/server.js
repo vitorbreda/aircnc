97e9c20d029be00cd8c5aef6a77d782e56254bef
Vitor Breda <vitor@olivasdigital.com.br>
2019-10-03 00:18:35 -0300

    Front-end do projeto 🖥

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
 const routes = require('./routes');
 
 const app = express();
@@ -9,7 +12,9 @@ mongoose.connect("mongodb+srv://vitor:vitor@cluster0-okxb6.mongodb.net/semana09?
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
