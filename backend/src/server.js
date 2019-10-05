80b0d45b25b1be5067b495f6b02c01129877015b
Vitor Breda <vitor@olivasdigital.com.br>
2019-10-04 21:10:45 -0300

    Aplicativo mobile 📲

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
 
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+io.on('connection', socket => {
+   console.log('User connected', socket.id); 
+});
 
 mongoose.connect("mongodb+srv://vitor:vitor@cluster0-okxb6.mongodb.net/semana09?retryWrites=true&w=majority", {
     useNewUrlParser: true,
@@ -17,4 +25,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
